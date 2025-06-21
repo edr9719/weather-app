e83f5a121bfee691deff9af976378cea303ed8c0
Edgar <edr9719@outlook.com>
2025-06-21 13:13:44 -0600

    Initial commit - Weather App

---
+++ script.js
@@ -0,0 +1,6 @@
+const city = document.querySelector('.city');
+const temp = document.querySelector('.temp');
+const humidity = document.querySelector('.humidity');
+const windSpeed = document.querySelector('.wind');
+const input = document.getElementById('input');
+const searchBtn = document.getElementById('search-btn');
